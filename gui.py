3268698ee0d2d216ebe781405125b09a5988089f
unknown <patrikwagner.uk@gmail.com>
2024-04-03 18:42:28 +0100

    Remove collector

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 # Function to load data from URLs with caching and additional error handling
-@st.cache(allow_output_mutation=True)
+@st.cache_data()
 def load_data(url):
     try:
         df = pd.read_csv(url)
@@ -18,20 +18,24 @@ def execute_app():
     st.set_page_config(layout="wide")
     st.sidebar.header("Database Selection and Actions")
     db_urls = {
-        "Database 1": "https://pwagner.eu.pythonanywhere.com/scripts/static/token_database.csv",
-        "Database 2": "https://pwagner.eu.pythonanywhere.com/scripts/static/token_database_all.csv"
+        "Premium Listings": "https://pwagner.eu.pythonanywhere.com/scripts/static/token_database.csv",
+        "All Launched Coins": "https://pwagner.eu.pythonanywhere.com/scripts/static/token_database_all.csv"
     }
     db_choice = st.sidebar.selectbox("Choose the database:", options=list(db_urls.keys()))
 
     # Button for refreshing data
     if st.sidebar.button("Refresh Data"):
         # This will clear the cache and force all cached functions to rerun
-        st.legacy_caching.clear_cache()
+        # st.legacy_caching.clear_cache()
         st.experimental_rerun()
 
+    col1, col2, col3
     # Display data from the selected database
     data = load_data(db_urls[db_choice])
     if not data.empty:
         st.dataframe(data)
     else:
         st.write("No data available or file not found.")
+
+if __name__ == "__main__":
+    execute_app()
