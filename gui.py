868ef7eac9b83bed5f8e0a919daaadf59269d13f
unknown <patrikwagner.uk@gmail.com>
2024-04-03 18:20:12 +0100

    Paths change 2

--- gui.py
+++ gui.py
@@ -1,44 +1,37 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
-import schedule
 
-from data_collector import collect_and_filter_data
+# Function to load data from URLs with caching and additional error handling
+@st.cache(allow_output_mutation=True)
+def load_data(url):
+    try:
+        df = pd.read_csv(url)
+        df.sort_values(by='timestamp', ascending=False, inplace=True)
+        return df
+    except (pd.errors.EmptyDataError, pd.errors.ParserError) as e:
+        st.error(f"Error loading data: {e}")
+        return pd.DataFrame()
 
 def execute_app():
-    # Function to load data from the selected CSV
-    def load_data(file_path):
-        try:
-            df = pd.read_csv(file_path)
-            df.sort_values(by='timestamp', ascending=False, inplace=True)
-            return df
-        except FileNotFoundError:
-            st.error("File not found. Please check if the data collector is running.")
-            return pd.DataFrame()
-
-    # Sidebar for database selection
+    # Sidebar for database selection and refresh button
     st.set_page_config(layout="wide")
-    st.sidebar.header("Database Selection")
-    db_choice = st.sidebar.selectbox("Choose the database:", ["token_database.csv", "token_database_all.csv"])
+    st.sidebar.header("Database Selection and Actions")
+    db_urls = {
+        "Database 1": "https://pwagner.eu.pythonanywhere.com/static/token_database.csv",
+        "Database 2": "https://pwagner.eu.pythonanywhere.com/static/token_database_all.csv"
+    }
+    db_choice = st.sidebar.selectbox("Choose the database:", options=list(db_urls.keys()))
+
+    # Button for refreshing data
+    if st.sidebar.button("Refresh Data"):
+        # This will clear the cache and force all cached functions to rerun
+        st.legacy_caching.clear_cache()
+        st.experimental_rerun()
 
     # Display data from the selected database
-    data = load_data(db_choice)
+    data = load_data(db_urls[db_choice])
     if not data.empty:
         st.dataframe(data)
     else:
         st.write("No data available or file not found.")
-
-    # Initialize or update the last refresh timestamp
-    if 'last_refresh' not in st.session_state:
-        st.session_state.last_refresh = datetime.now()
-
-    # Check if it's time to refresh (every 30 seconds)
-    if datetime.now() - st.session_state.last_refresh > timedelta(seconds=30):
-        st.session_state.last_refresh = datetime.now()
-        st.rerun()
-
-if __name__ == "__main__":
-
-    execute_app()
-    # Scheduling the data collection to run 20 times per minute
-    schedule.every(3).seconds.do(collect_and_filter_data)
