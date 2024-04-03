ed511f7857cf78e82b81762c89ea41c97d5d2453
unknown <patrikwagner.uk@gmail.com>
2024-04-03 18:47:27 +0100

    Formatting

--- gui.py
+++ gui.py
@@ -29,7 +29,10 @@ def execute_app():
         # st.legacy_caching.clear_cache()
         st.experimental_rerun()
 
-    col1, col2, col3
+    col1, col2, col3 = st.columns(3)
+
+    with col2:
+        st.title("New Liquidity Pools Monitor")
     # Display data from the selected database
     data = load_data(db_urls[db_choice])
     if not data.empty:
